ce81c0e429e820ff49f3c9c94663d9f3bda95adb
pass4 <fast.out1592@gmail.com>
2024-06-27 12:50:21 +0000

    update: wandbの設定を追加

--- examples/myscripts/train_dpo_llama_lora.sh
+++ examples/myscripts/train_dpo_llama_lora.sh
@@ -8,18 +8,26 @@ read -r -d '' training_commands <<EOF
      --eval_steps -1 \
      --train_batch_size 128 \
      --micro_train_batch_size 1 \
-     --pretrain OpenLLMAI/Llama-2-13b-sft-model-ocra-500k \
+     --pretrain /storage5/hf_private/model/hatakeyama-llm-team/Tanuki-8B-Instruct-without-DPO \
      --bf16 \
      --max_epochs 1 \
      --max_len 2048 \
      --zero_stage 3 \
      --beta 0.1 \
      --learning_rate 5e-7 \
-     --dataset Anthropic/hh-rlhf,tasksource/oasst1_pairwise_rlhf_reward,lmsys/chatbot_arena_conversations,openai/webgpt_comparisons \
-     --dataset_probs 0.72,0.08,0.12,0.08 \
+     --dataset Anthropic/hh-rlhf,tasksource/oasst1_pairwise_rlhf_reward,openai/webgpt_comparisons \
+     --dataset_probs 0.72,0.08,0.2 \
      --flash_attn \
      --gradient_checkpointing \
      --ref_offload
+     --lora_rank 8
+     --lora_alpha 16
+     --lora_dropout 0.05
+     --use_wandb true
+     --wandb_org weblab-geniac1
+     --wandb_project OpenRLHF-test
+     --wandb_group test
+     --wandb_run_name test
 EOF
      # --wandb [WANDB_TOKENS] or True (use wandb login command)
      # --ipo [for IPO]
