b3f4335bda1cd97182bc41557046650b1b454881
passsalt <fast.out1592@gmail.com>
2024-07-10 07:09:47 +0900

    Revert "For Phase2 GCP multi nodes testing"

--- examples/scripts/train_conditional_llama.sh
+++ examples/scripts/train_conditional_llama.sh
@@ -1,20 +1,19 @@
 set -x
 
-mkdir -p ./checkpoint/llama-2-8b-csft
-RM_OUTPUT=./checkpoint/llama-2-8b-csft/rm.jsonl
+mkdir -p ./ckpt/7b_llama_ca
+RM_OUTPUT=./ckpt/7b_llama_ca/rm.jsonl
 
 read -r -d '' get_rewards_commands <<EOF
 ../batch_inference.py
     --eval_task rm \
-    --pretrain OpenLLMAI/Llama-3-8b-rm-mixture \
+    --pretrain OpenLLMAI/Llama-2-7b-rm-anthropic_hh-lmsys-oasst-webgpt \
     --bf16 \
-    --max_len 4096 \
-    --dataset OpenLLMAI/preference_dataset_mixture2_and_safe_pku \
-    --input_key chosen \
-    --apply_chat_template \
+    --max_len 2048 \
+    --dataset Open-Orca/OpenOrca,Dahoas/full-hh-rlhf \
+    --dataset_probs 0.5,0.5 \
     --max_samples 128000 \
     --zero_stage 0 \
-    --post_processor csft \
+    --post_processor ca \
     --normalize_reward
     --micro_batch_size 4 \
     --output_path $RM_OUTPUT
@@ -22,13 +21,13 @@ EOF
 
 read -r -d '' sft_commands <<EOF
 ../train_sft.py \
-    --max_len 4096 \
+    --max_len 2048 \
     --dataset $RM_OUTPUT \
     --dataset_probs 1.0 \
     --train_batch_size 128 \
     --micro_train_batch_size 2 \
-    --pretrain OpenLLMAI/Llama-3-8b-sft-mixture \
-    --save_path ./checkpoint/llama-3-8b-csft \
+    --pretrain OpenLLMAI/Llama-2-7b-sft-model-ocra-500k \
+    --save_path ./ckpt/7b_llama_ca \
     --zero_stage 2 \
     --max_epochs 1 \
     --bf16 \
--- examples/scripts/train_sft_mixtral_lora.sh
+++ examples/scripts/train_sft_mixtral_lora.sh
@@ -4,13 +4,12 @@ read -r -d '' training_commands <<EOF
 ../train_sft.py \
     --max_len 2048 \
     --dataset Open-Orca/OpenOrca \
-    --input_key question \
-    --output_key response \
+    --dataset_probs 1.0 \
     --train_batch_size 128 \
     --micro_train_batch_size 4 \
     --max_samples 500000 \
     --pretrain mistralai/Mixtral-8x7B-v0.1 \
-    --save_path ./checkpoint/mixtral-sft-lora\
+    --save_path ./ckpt/mixtral_sft\
     --save_steps -1 \
     --logging_steps 1 \
     --eval_steps -1 \
