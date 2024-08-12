e714372f1cae013cf2fc7673a98a77406b53486a
Atsushi Saito <atsushi.saito.dec17@gmail.com>
2024-08-12 11:14:50 +0000

    Modified shells

--- examples/scripts/moe_dpo_slurm.sh
+++ examples/scripts/moe_dpo_slurm.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --time 20:00:00              # maximum execution time (HH:MM:SS)
-#SBATCH --nodelist slurm0-a3-ghpc-[5,6,7,8,9,10,11,12]
-
-#SBATCH --job-name=MnMoeDpo        # name
+#SBATCH --nodelist slurm0-a3-ghpc-[2,5-6,8-12]
 #SBATCH --nodes=8                    # nodes
+
+#SBATCH --job-name=MnDpo        # name
 #SBATCH --ntasks-per-node=1          # crucial - only 1 task per dist per node!
 #SBATCH --cpus-per-task=8            # number of cores per tasks
 #SBATCH --gres=gpu:8                 # number of gpus
@@ -25,25 +25,20 @@ export MASTER_PORT=9901
 
 #export OPENRLHF_LOAD_MODEL_NAME_OR_PATH="/storage5/shared/Nishijima/Llama-3-8b-MoE/5th_tonyu_iter_7200"
 # "/storage5/someya/outputs/sftlab-experiments/8B_iter126000/someya-exp007-zero1/checkpoint-2000"
-#export OPENRLHF_SAVE_MODEL_PATH="/storage5/saito/ckpt_moe_openrlhf"
-#export OPENRLHF_DATA_LIST="team-hatakeyama-phase2/applied-chat-templace-dpo-120k"
-# "team-hatakeyama-phase2/synth-dpo-calm22b-250k"
+export TRITON_CACHE_DIR="/storage5/.triton"
 
-export OPENRLHF_LOAD_MODEL_NAME_OR_PATH="/storage5/someya/outputs/sftlab-experiments/8x8B/someya-sft_013-zero3_multi_node_no_offload"
-# "/storage5/shared/Nishijima/Llama-3-8b-MoE/9th_tonyu_iter_0001200"
-# "/storage5/someya/outputs/sftlab-experiments/8x8B/someya-sft_004-zero3_multi_node"
-# "/storage5/shared/Nishijima/Llama-3-8b-MoE/5th_tonyu_iter_7200"
-# "/storage5/someya/outputs/sftlab-experiments/8B/someya-sft_011-zero1"
-export OPENRLHF_SAVE_MODEL_PATH="/storage5/saito/04_4node_ckpt_moe_openrlhf"
-export OPENRLHF_DATA_LIST="team-hatakeyama-phase2/dpo-oai-format-mixed-95k"
-# "team-hatakeyama-phase2/dpo-oai-format-mixed-224k"
-# "team-hataKeyama-phase2/dpo-nemotron-math-coding-reasoning-14k,team-hatakeyama-phase2/dpo-multiturn-rand-genre-60k-len-80,team-hatakeyama-phase2/dpo-singleturn-rand-genre-140k"
-# ",team-hatakeyama-phase2/dpo-singleturn-rand-genre-140k"
+export OPENRLHF_WANDB_NAME="weblab-geniac1"
+export OPENRLHF_WANDB_RUN="exp_"$(date '+%Y%m%d%H%S')
+export OPENRLHF_WANDB_PROJECT="openrlhf_train_dpo"
+
+# export OPENRLHF_LOAD_MODEL_NAME_OR_PATH="/storage5/someya/outputs/sftlab-experiments/8x8B/someya-sft_019-zero3_multi_node_no_offload/checkpoint-215"
+export OPENRLHF_LOAD_MODEL_NAME_OR_PATH="/storage5/someya/outputs/sftlab-experiments/8x8B/someya-sft_022-zero3_multi_node_no_offload/checkpoint-136"
+export OPENRLHF_TOKENIZER_REPO="team-hatakeyama-phase2/tanuki-tokenizer-without-space"
+export OPENRLHF_SAVE_MODEL_PATH="/storage5/saito/01_08nodes_ckpt_moe_openrlhf/"$OPENRLHF_WANDB_RUN
+export OPENRLHF_DATA_LIST="team-hatakeyama-phase2/dpo-oai-format-satoshi-ayaja-randgenre-80k"
 export OPENRLHF_DATA_PROB="1.0"
-# "team-hatakeyama-phase2/applied-chat-templace-dpo-small"
-# "team-hatakeyama-phase2/synth-dpo-calm22b-250k"
 
-
+mkdir -p $OPENRLHF_SAVE_MODEL_PATH
 # NOTE: eval_steps -1 means one evaluation per epoch
 
 srun --jobid $SLURM_JOBID bash -c 'python -m torch.distributed.run \
@@ -55,14 +50,16 @@ srun --jobid $SLURM_JOBID bash -c 'python -m torch.distributed.run \
      --save_steps 50 \
      --logging_steps 1 \
      --eval_steps 50 \
-     --micro_train_batch_size 1 \
+     --micro_train_batch_size 4\
      --train_batch_size 1024 \
      --pretrain $OPENRLHF_LOAD_MODEL_NAME_OR_PATH \
+     --tokenizer $OPENRLHF_TOKENIZER_REPO\
      --bf16 \
      --max_epochs 1 \
      --max_len 2048 \
      --zero_stage 3 \
-     --beta 0.1 \
+     --beta 0.5 \
+     --label_smoothing 0.01 \
      --learning_rate 1e-6 \
      --dataset $OPENRLHF_DATA_LIST \
      --dataset_probs $OPENRLHF_DATA_PROB \
@@ -70,7 +67,11 @@ srun --jobid $SLURM_JOBID bash -c 'python -m torch.distributed.run \
      --gradient_checkpointing \
      --adam_offload \
      --chosen_key chosen --rejected_key rejected \
-     --apply_chat_template
+     --apply_chat_template \
+     --use_wandb True \
+     --wandb_org $OPENRLHF_WANDB_NAME \
+     --wandb_project $OPENRLHF_WANDB_PROJECT \
+     --wandb_run_name $OPENRLHF_WANDB_RUN 
      '
 
 # sleep 60
